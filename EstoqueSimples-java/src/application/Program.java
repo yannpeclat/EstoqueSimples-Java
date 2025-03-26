f428a473c214ac21e4e6d9f8b568b200e1711262
yannpeclat <yannpeclat@gmail.com>
2025-03-26 09:57:53 -0300

    Add get e setter ( mesmo sem logica o sistema, porem add essas funnçoes) estarei depois ajustando todo o projeto usando o get/set e deixando o sistema mais logico

--- EstoqueSimples-java/src/application/Program.java
+++ EstoqueSimples-java/src/application/Program.java
@@ -16,8 +16,12 @@ public class Program {
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-
 		Product product = new Product(name, price);
+
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
 
 		System.out.println();
 		System.out.println("Product data: " + product);
--- EstoqueSimples-java/src/entities/Product.java
+++ EstoqueSimples-java/src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 
-	public String name;
-	public double price;
-	public int quantity;
+	private String name;
+	private double price;
+	private int quantity;
 
 	public Product() {
 	}
@@ -18,6 +18,26 @@ public class Product {
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
 	}
 
 	public double totalValueInStock() {
