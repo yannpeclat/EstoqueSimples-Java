309ee01462a704cace14490a893067112a4050cf
yannpeclat <yannpeclat@gmail.com>
2025-03-26 09:43:12 -0300

    Proposta de melhoria, usando sobrecarga

--- EstoqueSimples-java/src/entities/Product.java
+++ EstoqueSimples-java/src/entities/Product.java
@@ -6,10 +6,18 @@ public class Product {
 	public double price;
 	public int quantity;
 
+	public Product() {
+	}
+
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+	}
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
 	}
 
 	public double totalValueInStock() {
